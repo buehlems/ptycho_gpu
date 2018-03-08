3cbbb837c17cf7cbec0b83c9e77c5a3399e5aeeb
Florian Boßmann <bossmann@wa0222.dip.tu-dresden.de>
2018-03-08 18:26:16 +0100

    load phantom example data into PETSC

---
+++ load_phantom_example.py
@@ -0,0 +1,20 @@
+import ptychopy_1
+from ptychopy_1 import make_sp_cof, filters
+from scipy.io import loadmat
+import numpy as np
+import p2p as p2p
+
+mtx = loadmat('Python_testmatrix.mat')
+
+B = mtx['Y']
+f1 = filters('Ptycho',4,5)
+[l1, d1] = f1.shape
+f2 = np.array([])
+gridx = np.array(range(64))
+gridy = np.array([])
+mtype = 'periodic'
+
+A = make_sp_cof(l1, d1, gridx, f1, gridx.max()+1)
+X = np.zeros([A.shape[1],2*B.shape[1]])
+
+p2p.py2petsc(A.data,A.indices,A.indptr,B,X)
