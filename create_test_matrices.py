297fb5d2bf926fb536c87dc6da2471ee75216ffa
FBossmann <37180551+FBossmann@users.noreply.github.com>
2018-03-08 13:39:50 +0100

    added matrix X to fill in return values

--- create_test_matrices.py
+++ create_test_matrices.py
@@ -5,9 +5,10 @@ import p2p as p2p
 
 B = np.reshape(np.asarray(range(16))*0.5,[4,4])
 A = make_sp_cof(2,3,range(2),0.5*np.reshape(range(1,7),[2,3]),2)
+X = np.zeros([A.shape[1],B.shape[1]])
 print("types A=",type(A),"types A.data=",type(A.data),"types A.indices=",type(A.indices),"types A.indptr=",type(A.indptr),"types B=",type(B));
 print("types A.data[0]=",type(A.data[0]),"types A.indices[0]=",type(A.indices[0]),"types A.indptr[0]=",type(A.indptr[0]),"types B[0][0]=",type(B[0][0]));
-p2p.py2petsc(A.data,A.indices,A.indptr,B)
+p2p.py2petsc(A.data,A.indices,A.indptr,B,X)
 
 # print(A)
 print("A.data=",A.data)
