38cc6b421d0592483e5a5e667404b6f908c95f64
Florian Boßmann <bossmann@wa0222.dip.tu-dresden.de>
2018-03-09 09:01:30 +0100

    complex data array

--- create_test_matrices.py
+++ create_test_matrices.py
@@ -6,16 +6,19 @@ import p2p as p2p
 B = np.reshape(np.asarray(range(16))*0.5,[4,4])
 A = make_sp_cof(2,3,range(2),0.5*np.reshape(range(1,7),[2,3]),2)
 X = np.zeros([A.shape[1],2*B.shape[1]])
+A_data = np.zeros((2 * len(A.data)))
+A_data[0::2] = A.data
+
 print("types A=",type(A),"types A.data=",type(A.data),"types A.indices=",type(A.indices),"types A.indptr=",type(A.indptr),"types B=",type(B));
 print("types A.data[0]=",type(A.data[0]),"types A.indices[0]=",type(A.indices[0]),"types A.indptr[0]=",type(A.indptr[0]),"types B[0][0]=",type(B[0][0]));
-p2p.py2petsc(A.data,A.indices,A.indptr,B,X)
+p2p.py2petsc(A_data,A.indices,A.indptr,B,X)
 
-# print(A)
-print("A.data=",A.data)
-print("A.indices=",A.indices)
-print("A.iptr=",A.indptr)
-print("B=",B)
-print("X=",X)
+print(A)
+print("A.data=",A_data)
+#print("A.indices=",A.indices)
+#print("A.iptr=",A.indptr)
+#print("B=",B)
+#print("X=",X)
 
 # print("printNPY")
 # p2p.printNPY()
